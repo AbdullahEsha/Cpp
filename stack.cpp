7de880ecb234ed0e44a6dfdb3d04d2ea798da6dc
Shahriar Abdullah Isha <65135485+AbdullahIsha@users.noreply.github.com>
2021-06-27 16:02:00 +0600

    Add files via upload
    added switch case

--- stack.cpp
+++ stack.cpp
@@ -24,22 +24,71 @@ void pop()
         top--;
     }
 }
-int topElement(){
-    return st[top-1];
+int getTopElement(){
+    cout<<"Top Element is: "<<st[top-1]<<endl;
 }
-int main()
-{
-    push(3);
-    push(4);
-    pop();
-    push(1);
-    push(9);
-
-
-    for(int i=top-1; i>=0; i--)
+int print(){
+    cout<<"Elements are: \n";
+  for(int i=top-1; i>=0; i--)
     {
         cout<<st[i]<<"\n";
     }
-    cout<<"top element "<<topElement();
-
+}
+int main()
+{
+   //declare necessary variables and objects
+	bool repeat = true;
+	while(repeat==true)
+	{
+		cout<<"What do you want?"<<endl<<endl;
+		cout<<"1. Push an Element"<<endl;
+		cout<<"2. Pop an Element"<<endl;
+		cout<<"3. Get the top Element"<<endl;
+		cout<<"4. Print the stack"<<endl;
+		cout<<"5. Evaluate a Postfix Expression"<<endl;
+		cout<<"6. Exit"<<endl;
+		
+		int choice,x;
+		cin>>choice;
+		
+		switch(choice)
+		{
+			case 1:
+			    cout<<"Enter a value: ";
+				cin>>x;
+				push(x);
+				break;
+				
+			case 2:
+	            
+				pop();
+				break;
+				
+			case 3:
+			    
+				getTopElement();
+				break;
+				
+			case 4: 
+				
+				print();
+				break;
+				
+			case 5:
+			
+				//evaluateExpression();
+				break;
+				
+			case 6:
+			
+				repeat = false;
+				cout<<"You have choose to Exit...."<<endl;
+				break;
+			
+			default:
+				
+				cout<<"Invalid Choice..."<<endl;
+				break;
+		}
+	}
 }
